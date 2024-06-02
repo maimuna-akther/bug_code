ba7baebb2d382ca54c482d72bf887caac8ff0692
Maimuna Akther <157314363+maimuna-akther@users.noreply.github.com>
2024-06-02 22:51:18 +0600

    Create bug.c

---
+++ bug.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+    #include<stdlib.h>
+   
+      int sum(int n)
+   {
+      int s=0;
+      for(unsigned int i=1; i<=n;++i)
+    9    s+=i;
+   return s;
+   }
+ 
+    int main(int argc,char *argv[])
+  {
+    if (argc<2)
+  {
+    fprintf(stderr,"usage: ./bug [NUMBER]\n");
+    return -1;
+  }
+  
+   int n = atoi(argv[1]);
+  
+   /*
+   convert the string to integer = atoi
+   argv[1]="20"=20
+   stdout=standard output
+  */
+   printf("sum:%d\n",sum(n));
+   return 0;
+  }
+  
